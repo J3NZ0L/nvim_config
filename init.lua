c83156bd2e08d8a7cd9f067e53defbaea16e717d
zjeney <zoltanmarton.jeney@dxc.com>
2026-02-15 17:49:17 +0100

    fix clipboard issues

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.env.PATH = vim.env.PATH .. ":/usr/bin"
+
+vim.opt.clipboard = "unnamedplus"
+
 -- set the background to transparent
 vim.cmd([[
   highlight Normal guibg=none
